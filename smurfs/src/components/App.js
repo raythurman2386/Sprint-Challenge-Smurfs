908869af07f3205d3ed9df30f433fe84e47b5fee
raythurman2386 <raymondthurman5@gmail.com>
2019-10-27 19:34:10 -0400

    initial setup with redux hooks

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getSmurfs } from '../actions'
 import './App.css'
 
 const App = () => {
+  const dispatch = useDispatch()
+  const smurfs = useSelector(state => state.getSmurfs.smurfs)
+
+  useEffect(() => {
+    dispatch(getSmurfs())
+  }, [])
+
+  console.log(smurfs)
+
   return (
     <div className='App'>
       <h1>SMURFS! 2.0 W/ Redux</h1>
