f71320edb8e4dcefba3bbe639f6fa79755414c16
raythurman2386 <raymondthurman5@gmail.com>
2019-10-27 19:33:59 -0400

    setting smurf data to state

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -8,6 +8,6 @@ export const getSmurfs = () => dispatch => {
   dispatch({ type: GET_SMURFS_START })
   axios
     .get(`http://localhost:3333/smurfs`)
-    .then(res => console.log(res.data))
+    .then(res => dispatch({ type: GET_SMURFS_SUCCESS, payload: res.data }))
     .catch(err => dispatch({ type: GET_SMURFS_FAILED, payload: err.response }))
 }
