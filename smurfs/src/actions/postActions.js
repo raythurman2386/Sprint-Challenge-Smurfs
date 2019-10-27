afc60c038f0637b2729f6f668c0feebec20dd4d7
raythurman2386 <raymondthurman5@gmail.com>
2019-10-27 19:45:43 -0400

    initial post actions

---
+++ smurfs/src/actions/postActions.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const POST_SMURFS_START = 'GET_SMURFS_START'
+export const POST_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS'
+export const POST_SMURFS_FAILED = 'GET_SMURFS_FAILED'
+
+export const getSmurfs = data => dispatch => {
+  dispatch({ type: POST_SMURFS_START })
+  axios
+    .post(`http://localhost:3333/smurfs`)
+    .then(res => dispatch({ type: POST_SMURFS_SUCCESS, payload: data }))
+    .catch(err => dispatch({ type: POST_SMURFS_FAILED, payload: err.response }))
+}
