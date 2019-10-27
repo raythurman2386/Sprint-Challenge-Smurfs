525d8cb13d0ceba2ca716f9299569bb228331ad4
raythurman2386 <raymondthurman5@gmail.com>
2019-10-27 19:15:34 -0400

    basic redux boilerplate in place

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,5 +4,14 @@ import './index.css'
 import App from './components/App'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { reducer as smurfReducer } from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(smurfReducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,7 +2,7 @@ const initialState = {
   smurfs: []
 }
 
-export const reducer = (state = initialState, action) {
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     default:
       return state
