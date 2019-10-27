59555bbe4100abdad2376a52b061be246dc9e20e
raythurman2386 <raymondthurman5@gmail.com>
2019-10-27 19:23:59 -0400

    initial setup of getReducer

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const GET_SMURFS_START = 'GET_SMURFS_START'
+export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS'
+export const GET_SMURFS_FAILED = 'GET_SMURFS_FAILED'
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: GET_SMURFS_START })
+  axios
+    .get(`http://localhost:3333/smurfs`)
+    .then(res => console.log(res.data))
+    .catch(err => dispatch({ type: GET_SMURFS_FAILED, payload: err.response }))
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,9 +1,37 @@
+import {
+  GET_SMURFS_START,
+  GET_SMURFS_SUCCESS,
+  GET_SMURFS_FAILED
+} from '../actions'
+
 const initialState = {
-  smurfs: []
+  smurfs: [],
+  isLoading: false,
+  error: ''
 }
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_SMURFS_START:
+      return {
+        ...state,
+        smurfs: [],
+        isLoading: true
+      }
+    case GET_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isLoading: false,
+        error: ''
+      }
+    case GET_SMURFS_FAILED:
+      return {
+        ...state,
+        smurfs: [],
+        isLoading: false,
+        error: action.payload
+      }
     default:
       return state
   }
