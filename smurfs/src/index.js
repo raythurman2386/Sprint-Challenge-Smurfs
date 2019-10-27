6e6e7b13ba7e51a1eb09ddd5dc5ff7ee269a95b3
raythurman2386 <raymondthurman5@gmail.com>
2019-10-27 19:12:24 -0400

    basic reducer set up

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
