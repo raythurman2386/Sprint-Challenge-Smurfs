998a63c4dffca8f4401b7af42d4433852594a489
raythurman2386 <raymondthurman5@gmail.com>
2019-10-27 19:40:52 -0400

    removed logic from app

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,21 +1,10 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { getSmurfs } from '../actions'
+import React from 'react'
 
 import NewSmurf from './NewSmurf/NewSmurf'
 import SmurfList from './Smurfs/SmurfList'
 import './App.css'
 
 const App = () => {
-  const dispatch = useDispatch()
-  const smurfs = useSelector(state => state.getSmurfs.smurfs)
-
-  useEffect(() => {
-    dispatch(getSmurfs())
-  }, [])
-
-  console.log(smurfs)
-
   return (
     <div className='App'>
       <h1>SMURFS! 2.0 W/ Redux</h1>
