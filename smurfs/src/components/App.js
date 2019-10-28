5f511fd01594012f557a547c9122c919df6bf5d8
raythurman2386 <raymondthurman5@gmail.com>
2019-10-27 20:31:31 -0400

    removed app.css

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import NewSmurf from './NewSmurf/NewSmurf'
 import SmurfList from './Smurfs/SmurfList'
-import './App.css'
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>SMURFS! 2.0 W/ Redux</h1>
+    <AppWrapper>
+      <AppHeader>SMURFS! 2.0 W/ Redux</AppHeader>
       <SmurfList />
       <NewSmurf />
-    </div>
+    </AppWrapper>
   )
 }
 
 export default App
+
+const AppWrapper = styled.div`
+  text-align: center;
+  color: white;
+`
+const AppHeader = styled.header`
+  font-size: 3rem;
+  background-color: #111;
+  color: white;
+  height: 4rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
