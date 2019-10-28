3bdcb83d5560d9e0a55c0d0e7d335b5560c64cbc
raythurman2386 <raymondthurman5@gmail.com>
2019-10-27 20:31:45 -0400

    basic app styling, no images yet

--- smurfs/src/components/Smurfs/Smurf.js
+++ smurfs/src/components/Smurfs/Smurf.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Smurf = ({ smurf }) => {
   return (
-    <>
+    <SmurfWrapper>
       <h4>{smurf.name}</h4>
       <p>{smurf.age} Years Old</p>
       <p>{smurf.height}</p>
-    </>
+    </SmurfWrapper>
   )
 }
 
 export default Smurf
+
+const SmurfWrapper = styled.div`
+  background: #777;
+  margin: 2rem;
+  box-shadow: 0 0 15px black;
+  border-radius: 8px;
+  :hover {
+    box-shadow: 0 0 15px white;
+    transform: scale(1.1);
+  }
+`
--- smurfs/src/components/Smurfs/SmurfList.js
+++ smurfs/src/components/Smurfs/SmurfList.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getSmurfs } from '../../actions'
+import styled from 'styled-components'
 import Smurf from './Smurf'
 
 const SmurfList = () => {
@@ -12,11 +13,19 @@ const SmurfList = () => {
   }, [])
 
   return (
-    <div>
+    <SmurfWrapper>
       {smurfs &&
         smurfs.map((smurf, index) => <Smurf key={index} smurf={smurf} />)}
-    </div>
+    </SmurfWrapper>
   )
 }
 
 export default SmurfList
+
+const SmurfWrapper = styled.div`
+  max-width: 1280px;
+  margin: 40px auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-row-gap: 2rem;
+`
